8fb1375b5fa1d9d3695da22251ea177d4cd5126d
unknown <rainaz852@gmail.com>
2023-07-26 11:08:25 +0200

    init&& header

---
+++ my-project/pages/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import Head from "next/head";
+import { Hero } from "./../components/Hero";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>My portfolio</title>
+      </Head>
+      <main>
+        <Header />
+
+        <section id="hero">
+          <Hero />
+        </section>
+      </main>
+    </div>
+  );
+}
